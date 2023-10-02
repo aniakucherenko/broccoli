85d6167580a83557ca8ad3a0ca1d9004d628edab
aniakucherenko <ania.kucherenko@gmail.com>
2023-10-02 16:06:19 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
 // import App from 'components/App';
 // import './index.css';
 import StarRating from 'components/StarRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,6 +23,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
-    <StarRating size={30} color="pink" className="test" />
+    <StarRating size={30} color="pink" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
