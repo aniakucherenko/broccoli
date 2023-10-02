54a174c1e3168a0c1189ee795c2abb8ee00bf8d4
aniakucherenko <ania.kucherenko@gmail.com>
2023-10-02 16:21:22 +0200

    update PropTypes

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const containerStyle = {
   display: 'flex',
@@ -8,6 +9,15 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: 'flex',
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number.isRequired,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
